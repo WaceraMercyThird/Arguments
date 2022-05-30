f05e5be56c6a846ef3e66f5197f2c3368284fde3
Mercy Wacera <waceraaamercy@gmail.com>
2022-05-30 21:35:41 +0300

    kwargs

--- kwargs.py
+++ kwargs.py
@@ -1,6 +1,3 @@
-# Write a function that accepts any number of integers as positional arguments and any number of a student's attributes as keyword arguments and prints the result of multiplying all of the integers with a customized greeting based on the keyword arguments. Name the function multiply_and_greet.
- 
-
 
 def multiply_and_greet(*args, **kwargs):    
     product=1
@@ -10,7 +7,7 @@ def multiply_and_greet(*args, **kwargs):
 
     keys = kwargs.keys()
     if "age" in keys:
-        return f"Hello {kwargs['name']} you were born in {'year'}"
+        return f"Hello {kwargs['name']} you were born in {kwargs['year']}"
     elif "country" in keys:
         return f"Hello {kwargs['name']} from {kwargs['country']}"
     elif "name" in keys:
